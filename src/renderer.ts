acccd1ca7b46a381e576221d7bb97e7ef9d1f911
Shane Lippard <s.lippard@protonmail.com>
2018-12-10 12:43:21 -0500

    UI overhaul, Character creation & loading, pace & chances enabled

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,26 +1,75 @@
 import { Game } from './game';
+import { Character } from './models/character';
+import { Upgrade } from './schemas/upgrade';
+import DPlayer, { IPlayer } from './schemas/player';
+const { ipcRenderer } = require('electron');
 
 class App {
     private login: HTMLElement;
     private username: string;
     private loginBtn: Element;
     private app: HTMLElement;
-    constructor(doc: Document){
+    private character: IPlayer;
+    constructor() {
         this.login = (document.getElementsByClassName('login')[0] as HTMLInputElement);
         this.app = (document.getElementsByClassName('app')[0] as HTMLInputElement);
-        this.loginBtn = doc.getElementsByClassName('form__btn')[0];
+        this.loginBtn = document.getElementsByClassName('form__btn')[0];
+        
         this.loginBtn.addEventListener('click', (e) => {
             e.preventDefault();
             this.username = (document.getElementById('username') as HTMLInputElement).value;
             this.submit(this.username);
+        });
+
+        /*  this.atk__upgrade.addEventListener('click', function(e) {
+            var username = (document.getElementById('username') as HTMLInputElement).value;
+            return new Upgrade().upgrade_atk(username);
+        });  */
+
+        /* (document.getElementsByClassName('def__upgrade')[0] as HTMLElement).addEventListener('click', function() {
+            var username = (document.getElementById('player-name') as HTMLInputElement).value;
+            return new Upgrade().upgrade_def(username);
+        });
+
+        (document.getElementsByClassName('spd__upgrade')[0] as HTMLElement).addEventListener('click', function() {
+            var username = (document.getElementById('player-name') as HTMLInputElement).value;
+            return new Upgrade().upgrade_spd(username);
+        }); */
+/*
+        this.spd__upgrade.addEventListener('click', function(e) {
+            var username = (document.getElementById('username') as HTMLInputElement).value;
+            return new Upgrade().upgrade_spd(username);
         })
+
+        this.dex__upgrade.addEventListener('click', function(e) {
+            var username = (document.getElementById('username') as HTMLInputElement).value;
+            return new Upgrade().upgrade_dex(username);
+        })
+
+        this.vit__upgrade.addEventListener('click', function(e) {
+            var username = (document.getElementById('username') as HTMLInputElement).value;
+            return new Upgrade().upgrade_vit(username);
+        })
+
+        this.wis__upgrade.addEventListener('click', function(e) {
+            var username = (document.getElementById('username') as HTMLInputElement).value;
+            return new Upgrade().upgrade_wis(username);
+        }) */
+
+
+        ipcRenderer.on('createdchar', (event: any, char: IPlayer) => {
+            this.character = char;
+            console.log('char: ' + char.name);
+        })
+
     }
-    
-    private submit(player: string) {
-        if (!player) {
-            return console.log('No playername entered');
+
+    private submit(name: string) {
+        if (!name) {
+            return alert('No playername entered');
         } else {
-            new Game(player);
+            ipcRenderer.send('starting-game', name);
+            new Game(name, 300);
             this.login.style.opacity = '0';
             this.app.style.visibility = 'visible';
             this.app.style.opacity = '1';
@@ -29,6 +78,8 @@ class App {
             }, 1000);
         }
     }
+
+    
 }
 
-new App(document);
+new App();
--- src/turn.ts
+++ src/turn.ts
@@ -1,3 +1,6 @@
+import * as mongoose from 'mongoose';
+import DPlayer, { IPlayer } from './schemas/player';
+
 var Roll = require('roll'), roll = new Roll();
 var _ = require('lodash');
 
@@ -5,6 +8,7 @@ export class Turn {
 
     /* DOM ELEMENTS */
     private name: HTMLElement = (document.getElementsByClassName('player-name')[0] as HTMLInputElement);
+    private lvl: HTMLElement = (document.getElementsByClassName('xpbar__level')[0] as HTMLInputElement);
     private xp: HTMLElement = (document.getElementsByClassName('xpbar__value')[0] as HTMLInputElement);
     private fame: HTMLElement = (document.getElementsByClassName('famebar__value')[0] as HTMLInputElement);
     private atk: HTMLElement = (document.getElementsByClassName('atk__value')[0] as HTMLInputElement);
@@ -13,26 +17,22 @@ export class Turn {
     private def: HTMLElement = (document.getElementsByClassName('def__value')[0] as HTMLInputElement);
     private dex: HTMLElement = (document.getElementsByClassName('dex__value')[0] as HTMLInputElement);
     private wis: HTMLElement = (document.getElementsByClassName('wis__value')[0] as HTMLInputElement);
-    private lvl: HTMLElement = (document.getElementsByClassName('xpbar__level')[0] as HTMLInputElement);
 
     /* LOGIC */
-    private gold: Number;
-    private speed: Number;
-    private loop: Boolean;
-
-    constructor(player: any) {
-        this.loop = true;
-        while (this.loop = true) {
-            this.updateWindow(player);
-            this.action(player);
-        }
+    private player: any;
+    private gold: number;
+    private loop: boolean;
+    private speed: number;
+    constructor(player: IPlayer) {
+        console.log(player.name + ' starting turn');
+        this.startTurn(player);
     }
 
-    private async updateWindow(player: any) {
-        return await this.updateFields(player);
+    private startTurn(player: object) {
+        
     }
 
-    private updateFields(player: any) {
+    private async updateFields(player: any) {
         this.name.innerHTML = player.name;
         this.xp.innerHTML = player.xp;
         this.lvl.innerHTML = player.level;
@@ -43,39 +43,7 @@ export class Turn {
         this.vit.innerHTML = player.stats.vit;
         this.dex.innerHTML = player.stats.vit;
         this.wis.innerHTML = player.stats.wis;
+        return console.log('Main window updated');
     }
-
-    private async action(player: any) {
-        let output = roll.roll('1d100').result;
-        let player_speed = await this.getSpeed(player);
-        console.log('player speeD: ' + player_speed*100)
-        if (output >= 80) {
-            return await this.fight();
-        } else {
-            return await this.move(player, player_speed);
-        }
-    }
-
-    
-
-    private async fight() {
-        return console.log('Fought enemy');
-    }
-
-    private async getSpeed(player: any) {
-        this.speed = player.stats.spd;
-        let arr = [
-            player.stats.atk,
-            player.stats.def,
-            player.stats.spd,
-            player.stats.dex,
-            player.stats.vit,
-            player.stats.wis
-        ];
-        let pace = _.sum(arr);
-        /* let pace = Math.floor(this.speed + player.stats.atk + ) */
-        return pace;
-    }
-
 
 }
